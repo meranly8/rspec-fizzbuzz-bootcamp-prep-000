045b7c4d738472d1d0174b1768aec4cdb401ebf8
Michelle Ranly <meranly8@gmail.com>
2020-01-20 16:53:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # See README.md for instructions on how to do this
 def fizzbuzz
-  if int % 3 === 0 
+  if int % 3 == 0 
     puts "Fizz"
+  elsif int % 5 == 0 
+    puts "Buzz"
+  elsif int % 3 77 int % 5 == 0 
+    puts "FizzBuzz"
 end
